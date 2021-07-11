137b78419291e3e33111cf3ee4fd46036847cf12
Sara <sluisdejesus@gmail.com>
2021-07-11 11:53:44 +0100

    added imports to console.py

---
+++ console.py
@@ -0,0 +1,8 @@
+import pdb
+from models.member import Member
+from models.session import Session
+from models.booking import Booking
+
+import repositories.member_repository as member_repository
+import repositories.session_repository as session_repository
+import repositories.booking_repository as booking_repository
